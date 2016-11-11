dd4fa520ab3063fd3d9a398957c0345093d8153a
lilihao1993 <18166043874@163.com>
2016-11-11 18:26:03 +0800

    es

---
+++ src/main/java/config/InitES.java
@@ -0,0 +1,41 @@
+package config;
+
+
+import io.searchbox.client.JestClient;
+import io.searchbox.client.JestClientFactory;
+import io.searchbox.client.config.HttpClientConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 描述：初始化连接es服务端
+ */
+@Configuration
+public class InitES {
+
+
+    /**
+     * 配置jest客户端,到时使用spring时,可以用配置方式 ,现在暂时使用new ...
+     *
+     * @return
+     */
+    @Bean
+    public HttpClientConfig httpClientConfig() {
+        String connectionUrl = "http://localhost:9200";
+//        String connectionUrl = "http://192.168.5.115:9200";
+        HttpClientConfig httpClientConfig = new HttpClientConfig.Builder(connectionUrl).multiThreaded(true).build();
+        return httpClientConfig;
+    }
+
+    /**
+     * 描述：获取一个jest的对象
+     *
+     * @return
+     */
+    @Bean
+    public JestClient jestClient() {
+        JestClientFactory factory = new JestClientFactory();
+        factory.setHttpClientConfig(httpClientConfig());
+        return factory.getObject();
+    }
+}
